b348d494b0abbcba6de06e92d0ba539bef347682
Bibolat2005 <122878046+Bibolat2005@users.noreply.github.com>
2023-01-21 13:53:23 +0600

    Add files via upload

---
+++ Week 1 string formatting/62.py
@@ -0,0 +1,20 @@
+price = 49
+txt = "The price is {} dollars"
+print(txt.format(price))
+'''
+price = 49
+txt = "The price is {:.2f} dollars"
+print(txt.format(price))
+'''
+'''
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+'''
+'''
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname = "Ford", model = "Mustang"))
+
+'''

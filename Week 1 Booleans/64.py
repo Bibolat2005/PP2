e740395f399c6d0bde7177cf51ee8c2231a63f17
Bibolat2005 <122878046+Bibolat2005@users.noreply.github.com>
2023-01-21 14:02:22 +0600

    Add files via upload

---
+++ Week 1 Booleans/64.py
@@ -0,0 +1,39 @@
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+'''
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
+'''
+'''
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+'''
+'''
+def myFunction() :
+  return True
+
+print(myFunction())
+'''
+'''
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+'''
+'''
+x = 200
+print(isinstance(x, int))
+'''

1ece5257e753149679537d6024c8c8b9c15fbc29
Bibolat2005 <122878046+Bibolat2005@users.noreply.github.com>
2023-01-21 14:29:03 +0600

    Add files via upload

---
+++ Week 1 if and else/ex35.py
@@ -0,0 +1,14 @@
+'''
+if a == b and c == d:
+  print("Hello")
+if a == b or c == d:
+  print("Hello")
+
+
+if 5 > 2:
+   print("Five is greater than two!")
+'''
+
+if 5 > 2:print("Five is greater than two!")
+
+print("Yes") if 5 > 2 else print("No")

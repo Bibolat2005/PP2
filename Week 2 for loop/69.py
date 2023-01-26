7cd03aa7662d239f3a80303fb1df5c553a8fb886
Bibolat2005 <122878046+Bibolat2005@users.noreply.github.com>
2023-01-26 10:26:20 +0600

    Add files via upload

---
+++ Week 2 for loop/69.py
@@ -0,0 +1,24 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
+'''
+Loop through the letters in the word "banana":
+for x in "banana":
+  print(x) 
+'''
+'''
+Exit the loop when x is "banana":
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
+  if x == "banana":
+    break
+'''
+'''
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x) 
+'''
+
---
+++ Week 2 for loop/70.py
@@ -0,0 +1,37 @@
+for x in range(6):
+  print(x) 
+'''
+for x in range(2, 6):
+  print(x) 
+
+  
+for x in range(2, 30, 3):
+  print(x) 
+
+  
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+  
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+
+#If the loop breaks, the else block is not executed.
+
+
+#Print each adjective for every fruit:
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+    
+
+'''

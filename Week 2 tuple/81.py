3f4a9c640e126641fbe191383d7b5329fa2bdccb
Bibolat2005 <122878046+Bibolat2005@users.noreply.github.com>
2023-01-26 17:18:17 +0600

    Add files via upload

---
+++ Week 2 tuple/81.py
@@ -0,0 +1,35 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+'''
+#Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+#Tuples allow duplicate values:
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+
+#One item tuple, remember the comma:
+thistuple = ("apple",)
+print(type(thistuple))
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+
+tuple1 = ("abc", 34, True, 40, "male")
+
+
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+
+
+
+'''

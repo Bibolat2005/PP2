5ab09bc84dade8480a9602086ea4aaa4abb9c97a
Bibolat2005 <122878046+Bibolat2005@users.noreply.github.com>
2023-02-11 20:28:33 +0600

    Add files via upload

---
+++ Week 3 class and objects/ex71.py
@@ -0,0 +1,18 @@
+class Myclacc:
+    x=5
+
+class Myclass:
+    x=5
+p1=Myclass()
+
+
+class Myclass:
+    x=5
+p1=Myclass()
+print(p1.x)
+
+
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
